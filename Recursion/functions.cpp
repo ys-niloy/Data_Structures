6d2f8902632ad48c089d4195cc35d7d785e6afec
YS Niloy <115401989+ys-niloy@users.noreply.github.com>
2023-08-30 23:54:33 +0600

    Create functions.cpp

---
+++ Recursion/functions.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void reverse(int i, int arr[], int n)
+{
+    if(i == n/2)
+    {
+        return;
+    }
+    
+    swap(arr[i], arr[n-i-1]);
+    reverse(i+1, arr, n);
+}
+
+int main()
+{
+    int n = 5;
+    int arr[n] = {5,4,3,2,1};
+    reverse(0, arr, n);
+    cout << "Array after reversing: ";
+
+    for(int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
